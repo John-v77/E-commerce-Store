bfdd141c91aaa822ef54cd210333b6ea32b1f2c6
John-v77 <john.vs11@hotmail.com>
2022-07-11 13:16:59 -0400

    Navbar nostyle

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import Home from "./routes/home/home.component";
 import { Routes, Route } from "react-router-dom";
+import Navigation from "./routes/navigation.component";
+
+const Shop = () => {
+  return <h1>I am the shop page</h1>;
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/home" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+        {/* /home/shop */}
+      </Route>
     </Routes>
   );
 };
