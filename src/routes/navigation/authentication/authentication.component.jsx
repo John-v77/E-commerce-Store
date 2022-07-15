4c680784c4b59c26cd3cea674d176bad5643962b
John-v77 <john.vs11@hotmail.com>
2022-07-15 12:45:22 -0400

    context for login complete

--- src/routes/navigation/authentication/authentication.component.jsx
+++ src/routes/navigation/authentication/authentication.component.jsx
@@ -3,7 +3,6 @@ import SignUpForm from "../../../components/sign-up-form/sign-up-form.component"
 import SignInForm from "../../../components/sign-in-form/sign-in-form.component";
 
 const Authentication = (props) => {
-  console.log(props.not_registered);
   return (
     <div className="authentication-container">
       {props.not_registered ? <SignUpForm /> : <SignInForm />}
