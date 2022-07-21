373789b93f29433e14511a47fc4466e98e159f1d
John-v77 <john.vs11@hotmail.com>
2022-07-21 11:15:25 -0400

    add product to firebase store

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,24 +1,23 @@
-import { useContext } from 'react';
+import { useContext } from "react";
 
-import ProductCard from '../../components/product-card/product-card.component';
+import ProductCard from "../../components/product-card/product-card.component";
 
-import { ProductsContext } from '../../components/context/product.context';
+import { ProductsContext } from "../../components/context/products.context";
 
-import './shop.styles.scss';
+import "./shop.styles.scss";
 
-
-import PRODUCTS from '../../shop_data.json'
+import PRODUCTS from "../../shop_data.json";
 
 const Shop = () => {
-    const { products } = useContext(ProductsContext);
+  const { products } = useContext(ProductsContext);
 
-    return (
-        <div className='products_container'>
-            {PRODUCTS.map((product) => (
-                <ProductCard key={product.id} product={product} />
-            ))}
-        </div>
-    );
+  return (
+    <div className="products_container">
+      {PRODUCTS.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+  );
 };
 
 export default Shop;
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -13,7 +13,16 @@ import {
   onAuthStateChanged,
 } from "firebase/auth";
 
-import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  collection,
+  writeBatch,
+  query,
+  getDocs,
+} from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -95,4 +104,22 @@ export const signOutUser = async () => await signOut(auth);
 
 export const onAuthStateChangedListener = (callback) => {
   onAuthStateChanged(auth, callback);
+};
+
+// #4 Add Colection and Documents
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd,
+  field = "title"
+) => {
+  const collectionRef = collection(db, collectionKey);
+  const batch = writeBatch(db);
+
+  objectsToAdd.forEach((object) => {
+    const docRef = doc(collectionRef, object[field].toLowerCase());
+    batch.set(docRef, object);
+  });
+
+  await batch.commit();
+  console.log("done");
 };
