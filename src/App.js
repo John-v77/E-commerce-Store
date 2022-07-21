c4de1be89a7bf1b1b58011917078af53cfdc4001
John-v77 <john.vs11@hotmail.com>
2022-07-21 14:35:12 -0400

    reorganize routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import Home from "./routes/home/home.component";
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./routes/navigation/navigation.component.jsx";
-import Authentication from "./routes/navigation/authentication/authentication.component.jsx";
+import Authentication from "./routes/authentication/authentication.component.jsx";
 import Shop from "./routes/shop/shop.component";
-import Checkout from "./routes/navigation/checkout/checkout.component";
+import Checkout from "./routes/checkout/checkout.component.jsx";
 
 const App = () => {
-  
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path="shop" element={<Shop />} />
+        <Route path="shop/" element={<Shop />} />
         <Route path="auth" element={<Authentication />} />
         <Route path="register" element={<Authentication not_registered />} />
         <Route path="checkout" element={<Checkout />} />
