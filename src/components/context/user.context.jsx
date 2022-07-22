b87f569a79f92dd3b9b75439f3b8c82d495b3676
John-v77 <john.vs11@hotmail.com>
2022-07-22 15:39:37 -0400

    bring createAction into userContext

--- src/components/context/user.context.jsx
+++ src/components/context/user.context.jsx
@@ -1,8 +1,10 @@
 import { createContext, useEffect, useReducer } from "react";
+
 import {
   onAuthStateChangedListener,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
+import { createAction } from "../../utils/reducer/reducer.utils";
 
 // as the actual value you want to access
 export const UserContext = createContext({
@@ -43,7 +45,7 @@ export const UserProvider = ({ children }) => {
   // const [{ currentUser }, dispatch] = useReducer(userReducer, state);
 
   const setCurrentUser = (user) => {
-    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+    dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
   };
 
   const value = { currentUser, setCurrentUser };
