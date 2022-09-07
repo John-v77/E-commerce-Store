1c697772c43ab894ff361163e41cafb9c5ef9ca9
John-v77 <john.vs11@hotmail.com>
2022-09-07 14:05:48 -0400

    05 | stripe: payment form - first call to lambda function

---
+++ netlify/functions/create-payment-intent.js
@@ -0,0 +1,32 @@
+const { async } = require('@firebase/util');
+
+require('dotenv').config();
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+console.log(stripe, 'step 1 - stripe');
+
+exports.handler = async (event) => {
+  try {
+    // $100.00 => 10000
+    const { amount } = JSON.parse(event.body);
+
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amount,
+      currency: 'usd',
+      payment_method_types: ['card'],
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ paymentIntent }),
+    };
+  } catch (error) {
+    console.log(error);
+
+    return {
+      status: 400,
+      body: JSON.stringify({ error }),
+    };
+  }
+};
--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -1,6 +1,6 @@
-import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
-import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
-import { FormContainer, PaymentFormContainer } from "./payment-form.styles";
+import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
+import { FormContainer, PaymentFormContainer } from './payment-form.styles';
 
 const PaymentForm = () => {
   const stripe = useStripe();
@@ -12,15 +12,24 @@ const PaymentForm = () => {
     if (!stripe || !elements) {
       return;
     }
+
+    const response = await fetch('/.netlify/functions/create-payment-intent', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ amount: 70000 }),
+    }).then((res) => res.json());
+
+    console.log(response, 'new Response 321');
   };
 
   return (
     <PaymentFormContainer>
-      <FormContainer>
+      <FormContainer onSubmit={paymentHandler}>
         <h1>Credit Cart Payment</h1>
-        <CardElement>
-          <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay now</Button>
-        </CardElement>
+        <CardElement />
+        <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay now</Button>
       </FormContainer>
     </PaymentFormContainer>
   );
