bdd58efa056f846ddda2ce7372fe95cbe7ec52d8
John-v77 <john.vs11@hotmail.com>
2022-09-09 19:59:44 -0400

    06 | styled-comp: change cart-dropdown component

--- src/components/shoppingCart/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/shoppingCart/cart-dropdown/cart-dropdown.component.jsx
@@ -1,33 +1,38 @@
-import { useDispatch, useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
-import { setIsCartOpen } from '../../../store/cart/cart.actions'
-import { selectCartItems } from '../../../store/cart/cart.selector'
-import Button from '../../button/button.component'
-import CartItem from '../cart-item/cart-item.component'
-import './cart-dropdown.style.scss'
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { setIsCartOpen } from '../../../store/cart/cart.actions';
+import { selectCartItems } from '../../../store/cart/cart.selector';
+import Button from '../../button/button.component';
+import CartItem from '../cart-item/cart-item.component';
+import {
+  CartDropdownContainer,
+  CartItems,
+  EmptyMessage,
+} from './cart-dropdown.style';
 
 const CartDropdown = () => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
+  const cartItems = useSelector(selectCartItems);
+  const navigate = useNavigate();
 
+  const goToCheckoutHandler = () => {
+    dispatch(setIsCartOpen(false));
+    navigate('/checkout');
+  };
 
-    const cartItems = useSelector(selectCartItems)
-    const navigate = useNavigate()
+  return (
+    <CartDropdownContainer>
+      <CartItems>
+        {cartItems.length ? (
+          cartItems.map((item) => <CartItem key={item.id} CartItem={item} />)
+        ) : (
+          <EmptyMessage> Cart is empty</EmptyMessage>
+        )}
+      </CartItems>
+      <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
+    </CartDropdownContainer>
+  );
+};
 
-    const goToCheckoutHandler = () => {
-        dispatch(setIsCartOpen(false))
-        navigate('/checkout')
-    }
-
-    return (
-        <div className='cart-dropdown-container'>
-            <div className='cart-items'>
-                {cartItems.map(item => <CartItem key={item.id} CartItem={item} />)}
-            </div>
-            <Button onClick={goToCheckoutHandler} >GO TO CHECKOUT</Button>
-
-        </div>
-    )
-}
-
-export default CartDropdown
+export default CartDropdown;
