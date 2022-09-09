3d150b09d97ced4154d1ba4cd57f5940044e8a6f
John-v77 <john.vs11@hotmail.com>
2022-09-09 19:43:20 -0400

    06 | styled-comp: change button for product card

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,29 +1,33 @@
-import { useDispatch, useSelector } from "react-redux";
-import Button from "../button/button.component";
-import './product-card.styles.scss'
-import { addItemToCart } from '../../store/cart/cart.actions'
-import { selectCartItems } from "../../store/cart/cart.selector";
+import { useDispatch, useSelector } from 'react-redux';
+import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
+import './product-card.styles.scss';
+import { addItemToCart } from '../../store/cart/cart.actions';
+import { selectCartItems } from '../../store/cart/cart.selector';
 
 const ProductCard = ({ product }) => {
-    // const addItemToCart = useSelector(selec)
+  // const addItemToCart = useSelector(selec)
 
-    const { name, price, imageUrl } = product
-    const cartItems = useSelector(selectCartItems)
-    const dispatch = useDispatch()
+  const { name, price, imageUrl } = product;
+  const cartItems = useSelector(selectCartItems);
+  const dispatch = useDispatch();
 
-    const addProductToCart = () => dispatch(addItemToCart(cartItems, product))
+  const addProductToCart = () => dispatch(addItemToCart(cartItems, product));
 
-    return (
-        <div className="product-card-container" >
-
-            <img src={imageUrl} alt={`${name}`} />
-            <div className="footer">
-                <span className="name">{name}</span>
-                <span className="price">{price}</span>
-            </div>
-            <Button buttonType='inverted' onClick={addProductToCart} >Add to card</Button>
-        </div>
-    );
-}
+  return (
+    <div className='product-card-container'>
+      <img src={imageUrl} alt={`${name}`} />
+      <div className='footer'>
+        <span className='name'>{name}</span>
+        <span className='price'>{price}</span>
+      </div>
+      <Button
+        buttonType={BUTTON_TYPE_CLASSES.inverted}
+        onClick={addProductToCart}
+      >
+        Add to card
+      </Button>
+    </div>
+  );
+};
 
 export default ProductCard;
