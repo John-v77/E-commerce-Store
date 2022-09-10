c2ab0562f9dc7b12ebf3d3b315ff21a5f0441d4c
John-v77 <john.vs11@hotmail.com>
2022-09-10 10:57:32 -0400

    07 | reinstates the SignUp Form

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,13 +1,11 @@
-import "./authentication.style.scss";
-// import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
-import SignInForm from "../../components/sign-in-form/sign-in-form.component";
+import './authentication.style.scss';
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
+import SignInForm from '../../components/sign-in-form/sign-in-form.component';
 
 const Authentication = (props) => {
   return (
-    <div className="authentication-container">
-      {/* {props.not_registered ? <SignUpForm /> : <SignInForm />} */}
-
-      <SignInForm />
+    <div className='authentication-container'>
+      {props.not_registered ? <SignUpForm /> : <SignInForm />}
     </div>
   );
 };
