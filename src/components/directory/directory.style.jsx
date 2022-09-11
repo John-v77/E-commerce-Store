32608677f5335de1b8ba614ae4ba822d24048798
John-v77 <john.vs11@hotmail.com>
2022-09-11 14:19:36 -0400

    06 | styled-comp: directory component

---
+++ src/components/directory/directory.style.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const DirectoryContainer = styled.div`
+  width: 100%;
+  margin-top: 5rem;
+  padding: 0 1rem;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
