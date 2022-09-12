295364fa043123ca3fcba4ea44bf3d5d8c1a14a8
John-v77 <john.vs11@hotmail.com>
2022-09-12 18:41:50 -0400

    06 | styled-comp: cart-item component

--- src/components/shoppingCart/cart-item/cart-item.component.jsx
+++ src/components/shoppingCart/cart-item/cart-item.component.jsx
@@ -1,18 +1,19 @@
-import './cart-item.style.scss'
+import { CartItemContainer, ItemDetails, Name } from './cart-item.style.jsx';
 
 const CartItem = ({ CartItem }) => {
-    const { name, imageUrl, price, quantity } = CartItem
+  const { name, imageUrl, price, quantity } = CartItem;
 
-    return (
-        <div className='cart-item-container'>
-            <img src={imageUrl} alt={`${name}`} />
-            <div className='item-details'>
-                <h3 className='name'>{name}</h3>
-                <span>{quantity} x ${price} </span>
-            </div>
-        </div>
-    )
-
-}
+  return (
+    <CartItemContainer>
+      <img src={imageUrl} alt={`${name}`} />
+      <ItemDetails>
+        <Name>{name}</Name>
+        <span>
+          {quantity} x ${price}{' '}
+        </span>
+      </ItemDetails>
+    </CartItemContainer>
+  );
+};
 
 export default CartItem;
