12b648c6d893a5720b8c8cd83f1787b4779d9b08
John-v77 <john.vs11@hotmail.com>
2022-09-15 18:19:35 -0400

    07 | redux-saga : create user action functions

--- src/store/user/user.actions.js
+++ src/store/user/user.actions.js
@@ -1,6 +1,21 @@
-import { createAction } from "../../utils/reducer/reducer.utils";
-import { USER_ACTION_TYPES } from "./user.types";
+import { createAction } from '../../utils/reducer/reducer.utils';
+import { USER_ACTION_TYPES } from './user.types';
 
 export const setCurrentUser = (user) => {
   return createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user);
 };
+
+export const checkUserSession = () =>
+  createAction(USER_ACTION_TYPES.CHECK_USER_SESSION);
+
+export const googleSignInStart = () =>
+  createAction(USER_ACTION_TYPES.GOOGLE_SIGN_IN_START);
+
+export const emailSignInStart = () =>
+  createAction(USER_ACTION_TYPES.EMAIL_SIGN_IN_START);
+
+export const signInSuccess = () =>
+  createAction(USER_ACTION_TYPES.SIGN_IN_SUCCESS);
+
+export const signInFailure = () =>
+  createAction(USER_ACTION_TYPES.SIGN_IN_SUCCESS);
