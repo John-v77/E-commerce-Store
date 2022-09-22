43d8ce725ab4212892f36e3d8e9eb3fe1d0af080
John-v77 <john.vs11@hotmail.com>
2022-09-22 09:50:37 -0400

    adds max witdth to app

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ import Navigation from './routes/navigation/navigation.component.jsx';
 import Authentication from './routes/authentication/authentication.component.jsx';
 import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component.jsx';
-import { checkUserSession, setCurrentUser } from './store/user/user.actions';
+import { checkUserSession } from './store/user/user.actions';
 import { useDispatch } from 'react-redux';
+import './App.css';
 
 const App = () => {
   const dispatch = useDispatch();
@@ -17,16 +18,18 @@ const App = () => {
   }, []);
 
   return (
-    <Routes>
-      <Route path='/' element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path='shop/*' element={<Shop />} />
-        <Route path='auth' element={<Authentication />} />
-        <Route path='register' element={<Authentication not_registered />} />
-        <Route path='checkout' element={<Checkout />} />
-        {/* /home/shop */}
-      </Route>
-    </Routes>
+    <div className='App'>
+      <Routes>
+        <Route path='/' element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path='shop/*' element={<Shop />} />
+          <Route path='auth' element={<Authentication />} />
+          <Route path='register' element={<Authentication not_registered />} />
+          <Route path='checkout' element={<Checkout />} />
+          {/* /home/shop */}
+        </Route>
+      </Routes>
+    </div>
   );
 };
 
