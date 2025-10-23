2ff7c322962bba3612a56e721930ebe3aeeb832c
John-77 <john.vas131@gmail.com>
2025-10-23 17:52:51 -0400

    navitation test

--- src/routes/navigation/__test__/navigation.test.js
+++ src/routes/navigation/__test__/navigation.test.js
@@ -1,12 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import {store, persistor} from "../../../store/store"
+import { createStore, combineReducers } from 'redux';
 import Navigation from '../navigation.component';
+
+const mockStore = createStore(combineReducers({
+  cart: (state = { isCartOpen: false, cartItems: [] }) => state,
+  user: (state = { currentUser: null }) => state,
+  categories: (state = { categories: [] }) => state
+}));
 
 const MockTodo = () =>{
     return(
-        <Provider store={store}>
+        <Provider store={mockStore}>
             <BrowserRouter>
                 <Navigation />
             </BrowserRouter>
